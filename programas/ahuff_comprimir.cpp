2191ccd495e2ce897b3ee7a86c089cb9ca104b28
haztecaso <adrianlattes@disroot.org>
2021-12-28 18:20:44 +0100

    ahuff_comprimir terminado en un principio

--- programas/ahuff_comprimir.cpp
+++ programas/ahuff_comprimir.cpp
@@ -16,25 +16,15 @@ tabla_frecuencias<char> construir_tabla_frecuencias(ifstream &f){
     return tabla;
 }
 
+using ecod = entrada<char, codigo_h>;
 
-stringstream optimizar_codigo(vector<bool> codigo){
+stringstream codificar_tabla(tabla<char,codigo_h> tabla_codigos){
     stringstream s;
-    long num_bits = codigo.size();
-    unsigned char c;
-    long inic;
-    s << num_bits << endl;
-    for(int i = 0; i < num_bits/CHAR_BIT; i++){
-        c = 0;
-        inic = i*CHAR_BIT;
-        for(int j = CHAR_BIT-1; j>=0; j--) c = c*2+codigo[inic+j];
-        s<<c;
+    vector<ecod> v = vector<ecod>();
+    inorden(tabla_codigos, v);
+    for (ecod e: v){
+        s << e.clave << ":" << e.valor << ";";
     }
-    c = 0;
-    inic = (num_bits/CHAR_BIT)*CHAR_BIT;
-    for(int j = num_bits%CHAR_BIT-1;j>=0;j--){
-        c = c *2 + codigo[inic + j];
-    }
-    s << c;
     return s;
 }
 
@@ -50,36 +40,64 @@ vector<bool> comprimir(tabla<char,codigo_h> cods, ifstream &f){
     return datos;
 }
 
+stringstream optimizar_codigo(vector<bool> codigo){
+    stringstream s;
+    long num_bits = codigo.size();
+    unsigned char c;
+    long inic;
+    s << num_bits << ";";
+    for(int i = 0; i < num_bits/CHAR_BIT; i++){
+        c = 0;
+        inic = i*CHAR_BIT;
+        for(int j = CHAR_BIT-1; j>=0; j--) c = c*2+codigo[inic+j];
+        s<<c;
+    }
+    c = 0;
+    inic = (num_bits/CHAR_BIT)*CHAR_BIT;
+    for(int j = num_bits%CHAR_BIT-1;j>=0;j--){
+        c = c *2 + codigo[inic + j];
+    }
+    s << c;
+    return s;
+}
+
+
 int main(int argc, char** argv){
     if(argc != 2){
         std::cout << "Error: este programa requiere exactamente un parámetro, la dirección del fichero a comprimir." << endl;
         return 1;
     }
 
-    char * filename = argv[1];
+    string filename = string(argv[1]);
     ifstream f = ifstream(filename);
 
-    // Fichero -> Tabla de frecuencias de los caracteres
+    cout << "- Generando tabla de frecuencias desde el fichero " << filename << endl;
     tabla_frecuencias<char> frecs = construir_tabla_frecuencias(f);
 
-    // Tabla de frecuencias -> Árbol de Huffman
+    cout << "- Generando Árbol de Huffman" << endl;
     ahuff<char> a = ahuff_desde_frecuencias(frecs);
 
+    // Guarda el árbol de huffman en formato .dot. Muy útil para debuguear el
+    // código. Para compilar el gráfico hay que ejecutar el siguiente comando
+    // (del paquete graphviz): dot -Tpng -O ahuff.dot
+    cout << "- Generando ahuff.dot" << endl;
+    ahuff_graphviz("ahuff.dot", a);
 
-    // Árbol de Huffman -> tabla de códigos
+    cout << "- Generando tabla de códigos de Huffman" << endl;
     tabla<char,codigo_h> cods = tabla_vacia<char,codigo_h>();
     tabla_codigos(a, cods, codigo_vacio());
 
-    // Comprimir datos
-    vector<bool> datos = comprimir(cods, f);
+    cout << "- Comprimiendo fichero y codificando tabla de códigos" << endl;
+    stringstream codigos_codificados = codificar_tabla(cods);
+    vector<bool> codigo = comprimir(cods, f);
+    stringstream datos = optimizar_codigo(codigo);
 
-    stringstream s = optimizar_codigo(datos);
 
-    // Visualizar resultados
-    ahuff_graphviz("ahuff.dot", a);
-    // cout << "Tabla de frecuencias:\n" << frecs << "\n\n";
-    // cout << "Tabla de códigos:\n" << cods << "\n";
-    cout << "N: " << consultar(cods, 'N') << "\n";
-    cout << "Datos:\n" << s.str() << "\n";
+    string ofilename = filename + ".huff";
+    cout << "- Guardando fichero comprimido en " << ofilename << endl;
+    ofstream of = ofstream(ofilename);
+    of << codigos_codificados.rdbuf();
+    of << datos.rdbuf();
+
     return 0;
 }
